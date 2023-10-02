61ef5b2e3831cd791371b699b254eb78c67cde87
amaullah <amanullah@gmail.com>
2023-10-02 14:45:34 +0530

    final commit

--- src/app/constant.js
+++ src/app/constant.js
@@ -1,3 +1,3 @@
 export const ITEM_PER_PAGE = 9;
-// export const BASE_URL = 'https://ecomerce-back-end.vercel.app';
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'https://ecomerce-back-end.vercel.app';
+// export const BASE_URL = 'http://localhost:8080';
